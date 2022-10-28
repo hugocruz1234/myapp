9d50c73b0abf2c8990f63827f002b745acb66e9b
Ballo1988 <rounderjards@gmail.com>
2022-10-28 18:35:51 -0500

    First commit

---
+++ src/ImageExampleImage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+
+const ImageExampleImage = () => (
+  <Image src='https://1.bp.blogspot.com/-0kt6ekXtQrA/VQzbzppVR-I/AAAAAAACi6U/-8DXV8UxDKs/s1600/Atardecer%2Ben%2Bla%2Bplaya.jpg' size='small' />
+)
+
+export default ImageExampleImage
