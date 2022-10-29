b1c5c92e6c285f01ab8e4f0e2611f2df35cc1e21
Hugo Cruz <cruzhugo1997@gmail.com>
2022-10-28 19:34:57 -0500

    Modify const for function on the file ImageExampleImage.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,6 @@ import ImageExampleImage from './ImageExampleImage';
 
 test('renders learn react link', () => {
   render(<ImageExampleImage />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/ImageExampleImage.js
+++ src/ImageExampleImage.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Image } from 'semantic-ui-react'
 
-const ImageExampleImage = () => (
-  <Image src='https://1.bp.blogspot.com/-0kt6ekXtQrA/VQzbzppVR-I/AAAAAAACi6U/-8DXV8UxDKs/s1600/Atardecer%2Ben%2Bla%2Bplaya.jpg' size='small' />
-)
+function ImageExampleImage() {
+  return (
+      <div>
+        <a href='https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions'>
+          learn react      
+        </a>
+      </div>
+    );
+  }
+
+
 
 export default ImageExampleImage
