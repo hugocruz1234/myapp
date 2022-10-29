7edd1416c808b0c1a0b606e9028ee32497670927
Hugo Cruz <cruzhugo1997@gmail.com>
2022-10-28 19:10:53 -0500

    Eliminated learn react form the file App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,5 @@ import ImageExampleImage from './ImageExampleImage';
 
 test('renders learn react link', () => {
   render(<ImageExampleImage />);
-  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
