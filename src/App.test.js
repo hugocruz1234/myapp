b32a83192ee6b44d1de597ed7514dbca24d3c252
Hugo Cruz <cruzhugo1997@gmail.com>
2022-10-28 19:05:10 -0500

    Modify the import App to ImageExampleImage from the file App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ImageExampleImage from './ImageExampleImage';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<ImageExampleImage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
